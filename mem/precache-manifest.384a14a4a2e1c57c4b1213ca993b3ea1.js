0e5ea8000ca3b7cee75365f3234cabc32856b0d1
Oleg Novitsky <darkmus@tut.by>
2019-11-29 14:40:31 +0300

    mem

---
+++ mem/precache-manifest.384a14a4a2e1c57c4b1213ca993b3ea1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c8c63d3060a06eef8dd661df2a058f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3979506fba32c3b582fe",
+    "url": "/static/css/main.44e079d3.chunk.css"
+  },
+  {
+    "revision": "c2cd13b6da0b902d9aa1",
+    "url": "/static/js/2.6c3be0d8.chunk.js"
+  },
+  {
+    "revision": "3979506fba32c3b582fe",
+    "url": "/static/js/main.e709feaa.chunk.js"
+  },
+  {
+    "revision": "ab9c4e3aa1cc54bde82f",
+    "url": "/static/js/runtime-main.0b0aa4d3.js"
+  }
+]);
